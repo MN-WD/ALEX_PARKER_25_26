5e5bb60dc339370adbcb4f82abf1c68253d7f83e
MN <bmdaetl@gmail.com>
2025-09-25 17:18:51 +0200

    Structure MVC initiale

---
+++ app/config/params.php
@@ -0,0 +1,8 @@
+<?php
+// PARAMÈTRES PAR DÉFAUT
+
+// Paramètres de connexion à la DB
+define('DB_HOST', 'localhost:3306');
+define('DB_NAME', 'alex_parker_octobre_2021');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
---
+++ core/connexion.php
@@ -0,0 +1,8 @@
+<?php
+// CONNEXION
+
+try {
+    $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+} catch (PDOException $e) {
+    echo $e;
+}
