47dad9937dc204c93e69da9fae397ff66f3c433a
MN <bmdaetl@gmail.com>
2025-09-30 11:38:08 +0200

    Categories index

---
+++ app/views/categories/index.php
@@ -0,0 +1,9 @@
+<!-- Catégories -->
+<ul class="menu-link">
+    <?php
+    include_once '../app/models/categoriesModel.php';
+    $categories = \App\Models\CategoriesModel\findAll($db);
+    foreach($categories as $category) : ?>
+    <li><a href="#"><?php echo $category['name']; ?></a></li>
+    <?php endforeach; ?>
+</ul>
